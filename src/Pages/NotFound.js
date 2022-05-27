01c7c41792c2f4e11ce22b6935ca4bc06f70768d
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-05-27 22:00:28 +0600

    finishing 404 page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,7 +1,20 @@
 import React from "react";
+import ErrorBg from "../assets/error.jpg";
 
 const NotFound = () => {
-    return <div>page not found</div>;
+    return (
+        <div
+            className="w-full min-h-screen"
+            style={{
+                background: `url(${ErrorBg})`,
+                backgroundSize: "100%",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+            }}
+        >
+            <div className=""></div>
+        </div>
+    );
 };
 
 export default NotFound;
