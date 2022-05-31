3c7de16fa51db2201906a29c776a065ebf8e4286
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-05-31 18:06:40 +0600

    decrease functionality add

--- src/Components/UpdateOrderQuantity/UpdateOrderQuantity.js
+++ src/Components/UpdateOrderQuantity/UpdateOrderQuantity.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 import { toast } from "react-toastify";
 
 const UpdateOrderQuantity = ({ tool }) => {
-    const [disableIncrease, setDisableIncrease] = useState(false);
-    const [disableDiscrese, setDisableDiscrese] = useState(false);
-
     const {
         _id,
         availableQuantity,
@@ -32,27 +31,40 @@ const UpdateOrderQuantity = ({ tool }) => {
             })
                 .then((res) => res.json())
                 .then((result) => {
-                    toast("Item Deliverd successfully");
+                    toast("Item Increased");
                 });
-        } else {
-            setDisableIncrease(true);
         }
     };
     const handleDecreaseBtn = () => {
-        console.log("decrease");
+        const { orderQuantity, ...restTool } = tool;
+        if (orderQuantity) {
+            const newQuantity = orderQuantity - 1;
+            const newTool = { orderQuantity: newQuantity, ...restTool };
+            const url = `http://localhost:5000/order/${_id}`;
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(newTool),
+            })
+                .then((res) => res.json())
+                .then((result) => {
+                    toast("Item Decreased");
+                });
+        }
     };
     return (
         <div className="w-full kit-shadow  p-4 mt-10 rounded-lg flex justify-center">
             <div className="flex flex-col sm:flex-row ">
                 <button
-                    className={
-                        "btn btn-sm bg-secondary hover:bg-secondary hover:text-neutral border-none text-base-100 font-light"
-                    }
-                    onClick={handleIncreaseBtn}
-                    disabled={orderQuantity >= availableQuantity}
+                    className="btn btn-sm bg-secondary hover:bg-secondary hover:text-neutral border-none text-base-100 font-light"
+                    onClick={handleDecreaseBtn}
+                    disabled={orderQuantity <= minOrderQuantity}
                 >
-                    increase
+                    <FontAwesomeIcon icon={faMinus} className=" w-4 h-4" />
                 </button>
+
                 <input
                     type="text"
                     value={orderQuantity}
@@ -60,10 +72,13 @@ const UpdateOrderQuantity = ({ tool }) => {
                     className="input input-sm input-bordered w-28 sm:mx-2 mx-0 my-2 sm:my-0 text-center text-sm"
                 />
                 <button
-                    className="btn btn-sm bg-secondary hover:bg-secondary hover:text-neutral border-none text-base-100 font-light"
-                    onClick={handleDecreaseBtn}
+                    className={
+                        "btn btn-sm bg-secondary hover:bg-secondary hover:text-neutral border-none text-base-100 font-light"
+                    }
+                    onClick={handleIncreaseBtn}
+                    disabled={orderQuantity >= availableQuantity}
                 >
-                    decrease
+                    <FontAwesomeIcon icon={faPlus} className=" w-4 h-4" />
                 </button>
             </div>
         </div>
